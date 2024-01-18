8db026e4d5b6d986c6fae83fda8614da5d143e5f
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-18 22:38:05 +0000

    CFB10.3

---
+++ aula10.3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace aula10._3
+{
+    internal class Program
+    {
+        enum DiasSemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };
+        static void Main()
+        {
+
+            int ds = (int)DiasSemana.Sexta;
+
+            Console.WriteLine(ds);
+        }
+    }
+}
